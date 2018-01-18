f6ec9175ee9d9d9afe9eb642104de13e8e8564bb
Alli Tzadok <allisontzadok@gmail.com>
2018-01-18 17:17:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/(\d{3})-(\d{3})-(\d{4})/) ? true : false
+  phone.match(/([0-9] *{10})) ? true : false
 
+  valid_numbers = ["2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"]
 end
